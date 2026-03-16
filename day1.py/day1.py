695b3baaff6d72ab553f24af67a778bff1050158
U Sathwika Jain <usathwikajain.22@gmail.com>
2026-03-16 20:46:06 +0530

    Delete day1.py directory

--- day1.py/day1.py
+++
@@ -1,7 +0,0 @@
-# Program to print the integer entered by the user
-
-# Step 1: Take input from the user
-number = int(input("Enter an integer: "))
-
-# Step 2: Print the entered number
-print("You entered:", number)
