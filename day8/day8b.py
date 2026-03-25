135aa96187100e20bee32cfcd708186710afc449
Sathwikajain <usathwikajain.22@gmail.com>
2026-03-25 20:15:29 +0530

    day8 logic building

---
+++ day8/day8b.py
@@ -0,0 +1,13 @@
+'''# Pattern 8'''
+n = 5
+for i in range(1, n + 1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+
+'''Output:
+1 
+2 2 
+3 3 3
+4 4 4 4
+5 5 5 5 5'''
---
+++ day8/day8c.py
@@ -0,0 +1,13 @@
+'''# Pattern 9'''
+n = 5
+for i in range(n, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+'''Output:
+1 2 3 4 5 
+1 2 3 4 
+1 2 3
+1 2
+1'''
