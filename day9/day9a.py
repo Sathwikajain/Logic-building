e0ab2a1dc558c048f9b581b6156de02471ab4776
Sathwikajain <usathwikajain.22@gmail.com>
2026-03-27 22:32:21 +0530

    day 9 logic building

---
+++ day9/day9a.py
@@ -0,0 +1,17 @@
+# Pattern 1
+
+n = 4
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    for j in range(2 * (n - i)):
+        print(" ", end=" ")
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    
+    print()
+
+'''output:
+1 2         2 1 
+1 2 3     3 2 1
+1 2 3 4 4 3 2 1'''
