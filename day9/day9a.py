9a3933d91e1a61db7519fa58e18a0788124dbf9f
U Sathwika Jain <usathwikajain.22@email.com>
2026-03-29 12:22:09 +0530

    Updated day9 and cleaned day1 files

--- day9/day9a.py
+++ day9/day9a.py
@@ -1,5 +1,4 @@
 # Pattern 1
-
 n = 4
 for i in range(1, n + 1):
     for j in range(1, i + 1):
@@ -8,7 +7,6 @@ for i in range(1, n + 1):
         print(" ", end=" ")
     for j in range(i, 0, -1):
         print(j, end=" ")
-    
     print()
 
 '''output:
