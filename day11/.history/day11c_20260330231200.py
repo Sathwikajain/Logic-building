c589e9ccd86baaebfff62f50dba4876a3b5703ec
U Sathwika Jain <usathwikajain.22@email.com>
2026-03-30 23:15:10 +0530

    logic building

---
+++ day11/.history/day11c_20260330231200.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number: "))
+total = 0
+for i in range(1, n + 1):
+    total += i
+print("The sum of the first", n, "natural numbers is", total)
---
+++ day11/.history/day11c_20260330231410.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a number: "))
+total = 0
+for i in range(1, n + 1):
+    total += i
+print("The sum of the first", n, "natural numbers is", total)
+
+
+
+'''output:
+Enter a number: 6
+The sum of the first 6 natural numbers is 21'''
