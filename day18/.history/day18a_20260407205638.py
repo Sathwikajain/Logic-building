e31516017ab8e7abd9bab58ac38bf7cef6fedd8b
U Sathwika Jain <usathwikajain.22@email.com>
2026-04-07 21:01:05 +0530

    day18 logic

---
+++ day18/.history/day18a_20260407205638.py
@@ -0,0 +1,13 @@
+n = 5
+
+# Upper part
+for i in range(1, n + 1):
+    print("* " * i, end="")
+    print("  " * (2 * (n - i)), end="")
+    print("* " * i)
+
+# Lower part
+for i in range(n - 1, 0, -1):
+    print("* " * i, end="")
+    print("  " * (2 * (n - i)), end="")
+    print("* " * i)
