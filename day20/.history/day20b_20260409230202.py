76592c45f3ea0aff246c4bff7a442cfafdb52c10
U Sathwika Jain <usathwikajain.22@email.com>
2026-04-09 23:07:28 +0530

    day20 logic build

---
+++ day20/.history/day20b_20260409230202.py
@@ -0,0 +1,15 @@
+n = 3
+
+for i in range(1, n + 1):
+    # Spaces
+    print(" " * (n - i), end="")
+    
+    # Increasing
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    
+    # Decreasing
+    for j in range(i - 1, 0, -1):
+        print(j, end=" ")
+    
+    print()
---
+++ day20/day20c.py
@@ -0,0 +1,12 @@
+n = 4
+for i in range(n):
+    for j in range(n):
+        print((i + j) % 2, end=" ")
+    print()
+
+
+'''output:
+0 1 0 1 
+1 0 1 0 
+0 1 0 1
+1 0 1 0  '''   
